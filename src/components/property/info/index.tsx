be465b63c2b0e3d239b65068ff8d1334c7c03570
hvoking <gus@ugeom.com>
2024-07-13 02:32:01 -0400

    backup 13-07-24

--- src/components/property/info/index.tsx
+++ src/components/property/info/index.tsx
@@ -2,7 +2,17 @@ export const Info = ({ propertyInfo }: any) => {
 
 	return (
 		<div style={{fontSize: "1.6em", color: "rgba(0, 0, 0, 1)"}}>
-			{propertyInfo.nome}
+			<div>{propertyInfo.nome}</div>
+			<div style={{display: "grid", gridTemplateColumns: "1fr 1fr"}}>
+				<img src={propertyInfo.urls[0]} alt="cover-img" width="100%"/>
+				<div style={{display: "grid", gridTemplateColumns: "1fr 1fr"}}>
+					<img src={propertyInfo.urls[1]} alt="cover-img" width="100%"/>
+					<img src={propertyInfo.urls[2]} alt="cover-img" width="100%"/>
+					<img src={propertyInfo.urls[3]} alt="cover-img" width="100%"/>
+					<img src={propertyInfo.urls[4]} alt="cover-img" width="100%"/>
+				</div>
+			</div>
+
 		</div>
 	)
 }
