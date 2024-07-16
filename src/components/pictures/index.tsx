b4fcac7ad3da691cbf8c8349c6b51fd0bc28578a
hvoking <gus@ugeom.com>
2024-07-16 01:41:55 -0400

    backup 16-07-24

--- src/components/pictures/index.tsx
+++ src/components/pictures/index.tsx
@@ -2,31 +2,48 @@
 import { useState } from 'react';
 
 // App imports
-import { PropertyImages } from './body';
 import { Cross } from './cross';
+import { ArrowLeft } from './left';
+import { ArrowRight } from './right';
 import './styles.scss';
 
 // Context imports
 import { useTooltip } from '../context/maps/tooltip';
 
 export const Pictures = ({ setActivePictures }: any) => {
-	const [ currentImage, setCurrentImage ] = useState(0);
-	const { propertyInfo, activePropertyInfo, setActivePropertyInfo } = useTooltip();
+	const [ currentImage, setCurrentImage ] = useState(0);	
+	const { propertyInfo } = useTooltip();
+
+	const decrement = (e: any) => {
+		if(currentImage <= 0) {
+			return;
+		}
+		setCurrentImage(currentImage - 1);
+	}
+
+	const increment = (e: any) => {
+		if (currentImage === propertyInfo.urls.length - 1) {
+			return;
+		}
+		setCurrentImage(currentImage + 1);
+	}
+
 	
 	if (!propertyInfo) return <></>
-	const imagesLength = propertyInfo.urls.length;
 
 	return (
 		<div className="pictures-wrapper">
 			<Cross setActivePictures={setActivePictures}/>
-			<PropertyImages 
-				propertyInfo={propertyInfo}
-				imagesArray={propertyInfo.urls}
-				currentImage={currentImage} 
-				setCurrentImage={setCurrentImage}
-				imagesLength={imagesLength}
-			/>
+			<div className="pictures-body">
+				<ArrowLeft decrement={decrement}/>
+				<img 
+					className="pictures-item"
+					src={propertyInfo.urls[currentImage]}
+					alt="property-image"
+				/>
+				<ArrowRight increment={increment}/>
 			</div>
+		</div>
 	)
 }
 
