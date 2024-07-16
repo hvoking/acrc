487c63689a3d5684ea2dd1565ec5f3acd8a57052
hvoking <gus@ugeom.com>
2024-07-15 22:15:46 -0400

    backup 15-07-24

--- src/components/maps/logo/index.tsx
+++ src/components/maps/logo/index.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
 	return (
 		<img 
 			className="logo-acrc-mapa"
-			src={process.env.PUBLIC_URL + '/static/logos/logo.png'}
+			src={process.env.PUBLIC_URL + '/static/logos/logo.svg'}
 			alt="logo-acrc" 
 			width={"100px"}
 		/>
