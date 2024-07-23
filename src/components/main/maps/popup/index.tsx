2126aae2caa73aa88250ff59856be6aeae047f02
hvoking <gus@ugeom.com>
2024-07-23 03:38:34 -0400

    testing popup images

--- src/components/main/maps/popup/index.tsx
+++ src/components/main/maps/popup/index.tsx
@@ -8,45 +8,39 @@ import './styles.scss';
 import { Popup } from 'react-map-gl';
 
 export const CustomPopup: any = ({ marker, setPropertyInfo, setPropertyHoverInfo }: any) => {
-  const coordinates = marker.coordinates.split(",");
-  const latitude = parseFloat(coordinates[0]);
-  const longitude = parseFloat(coordinates[1]);
+	const coordinates = marker.coordinates.split(",");
+  const latitude = coordinates[0];
+	const longitude = coordinates[1];
 
   const [imageLoaded, setImageLoaded] = useState(false);
-  const [preloadedImage, setPreloadedImage] = useState<any>(null);
 
   useEffect(() => {
     const img = new Image();
     img.src = marker.imgUrl;
     img.onload = () => {
-      setPreloadedImage(img);
       setImageLoaded(true);
     };
-  }, [marker.imgUrl]);
+  }, [ marker.imgUrl ]);
 
   const onClick = () => setPropertyInfo(marker);
   const onClose = () => setPropertyHoverInfo(null);
-
+  
   return (
-    <Popup
-      longitude={longitude}
-      latitude={latitude}
-      closeOnClick={false}
-      onClose={onClose}
-      offset={20}
-    >
-      <div className="tooltip-wrapper" onClick={onClick}>
-        {imageLoaded ? (
-          <>
-            <img className="card-thumbnail" src={preloadedImage.src} alt="property"/>
-            <Description marker={marker} />
-          </>
-        ) : (
-          <div className="loading-placeholder">Loading...</div>
-        )}
-      </div>
-    </Popup>
-  );
-};
+      imageLoaded && (
+        <Popup
+          longitude={longitude}
+          latitude={latitude}
+          closeOnClick={false}
+          onClose={onClose}
+          offset={20}
+        >
+          <div className="tooltip-wrapper" onClick={onClick}>
+            <img className="card-thumbnail" src={marker.imgUrl} alt="property"/>
+            <Description marker={marker}/>
+          </div>
+        </Popup>
+      )
+    );
+}
 
-CustomPopup.displayName = "CustomPopup";
+CustomPopup.diplayName="CustomPopup";
