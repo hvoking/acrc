fb0f5910ad46db43183d4accb6c840c8da299deb
hvoking <gus@ugeom.com>
2024-07-23 03:34:46 -0400

    testing popup images

--- src/components/main/card/description/images/index.tsx
+++ src/components/main/card/description/images/index.tsx
@@ -3,17 +3,11 @@ import './styles.scss';
 
 export const Images = ({ propertyInfo, setActivePictures }: any) => {
 	const urls = propertyInfo.urls;
+	const onClick = () => setActivePictures(true);
 	
 	return (
-		<div 
-			className="property-images" 
-			onClick={() => setActivePictures(true)}
-		>
-		    <img
-		    	className="cover-img"
-		    	src={urls[0]} 
-		    	alt="cover-img" 
-		    />
+		<div className="property-images" onClick={onClick}>
+		    <img className="cover-img" src={urls[0]} alt="cover-img"/>
 		    <div className="property-secondary-images">
 		        <img src={urls[1]} alt="secondary-img" className="secondary-img"/>
 		        <img src={urls[2]} alt="secondary-img" className="secondary-img"/>
--- src/components/main/maps/popup/index.tsx
+++ src/components/main/maps/popup/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 // App imports
 import { Description } from './description';
 import './styles.scss';
@@ -5,25 +7,46 @@ import './styles.scss';
 // Third-party imports
 import { Popup } from 'react-map-gl';
 
-export const CustomPopup = ({ marker, setPropertyInfo, setPropertyHoverInfo }: any) => {
-	const coordinates = marker.coordinates.split(",");
-  const latitude = coordinates[0];
-	const longitude = coordinates[1];
-  
+export const CustomPopup: any = ({ marker, setPropertyInfo, setPropertyHoverInfo }: any) => {
+  const coordinates = marker.coordinates.split(",");
+  const latitude = parseFloat(coordinates[0]);
+  const longitude = parseFloat(coordinates[1]);
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [preloadedImage, setPreloadedImage] = useState<any>(null);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = marker.imgUrl;
+    img.onload = () => {
+      setPreloadedImage(img);
+      setImageLoaded(true);
+    };
+  }, [marker.imgUrl]);
+
+  const onClick = () => setPropertyInfo(marker);
+  const onClose = () => setPropertyHoverInfo(null);
+
   return (
     <Popup
       longitude={longitude}
       latitude={latitude}
       closeOnClick={false}
-      onClose={() => setPropertyHoverInfo(null)}
+      onClose={onClose}
       offset={20}
     >
-      <div className="tooltip-wrapper" onClick={() => setPropertyInfo(marker)}>
-        <img className="card-thumbnail" src={marker.imgUrl} alt="property"/>
-        <Description marker={marker}/>
+      <div className="tooltip-wrapper" onClick={onClick}>
+        {imageLoaded ? (
+          <>
+            <img className="card-thumbnail" src={preloadedImage.src} alt="property"/>
+            <Description marker={marker} />
+          </>
+        ) : (
+          <div className="loading-placeholder">Loading...</div>
+        )}
       </div>
     </Popup>
   );
-}
+};
 
-CustomPopup.diplayName="CustomPopup";
+CustomPopup.displayName = "CustomPopup";
