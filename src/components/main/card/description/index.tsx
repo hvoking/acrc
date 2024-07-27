3546c5ebf5a758053d0373aae0d4c7a7c24d7cfa
hvoking <gus@ugeom.com>
2024-07-27 07:25:04 -0400

    testing images on mobile

--- src/components/main/card/description/index.tsx
+++ src/components/main/card/description/index.tsx
@@ -6,17 +6,15 @@ import './styles.scss';
 // Third-party imports
 import * as d3 from 'd3';
 
-const siFormat = d3.format(",");
-
 export const Description = ({ propertyInfo, setActivePictures }: any) => {
+	const siFormat = d3.format(",");
+	const formatPrice = siFormat(propertyInfo.valorvenda).replaceAll(",", ".");
+	
 	return (
 		<div className="info-wrapper">
-		    <Images 
-		        propertyInfo={propertyInfo} 
-		        setActivePictures={setActivePictures}
-		    />
+		    <Images propertyInfo={propertyInfo} setActivePictures={setActivePictures}/>
 		    <div className="property-subtitle">
-		        Unidades a partir de R$ {siFormat(propertyInfo.valorvenda).replaceAll(",", ".")}
+		        Unidades a partir de R$ {formatPrice}
 		    </div>
 		    <div className="warning-button">
 		        ENTREGA 07/2026
